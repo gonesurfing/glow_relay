a165eff10fe33ee21d63cf50ccc820bbe0829926
Eric Thornton <ewthornton@gmail.com>
2023-11-04 13:16:05 -0400

    update comments/cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,29 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-// array of glow times for each 8bit ADC value
+// Array of glow times (seconds) mapped for each 8bit ADC value
 const uint8_t glow_data[256] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6,6,6,6,6,6,6,6,6,6,6,6,7,7,7,7,7,7,7,7,8,8,8,8,8,8,8,9,9,9,9,9,10,10,10,10,11,11,11,11,12,12,12,13,13,14,14,15,15,16,16,17,17,18,19,20,20,21,22,23,25,26,27,29,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30};
-uint8_t crank = 0;
+
+// Below two are volatile since they can be modified in an ISR
+volatile uint8_t crank = 0;
 volatile uint32_t timer0_millis;
 
-ISR(PCINT0_vect) {
+// Define an ISR for the crank signal
+ISR( PCINT0_vect ) {
+  // Set the crank flag to 1
   crank = 1;
 }
 
-ISR(TIMER0_COMPA_vect) {
+ISR( TIMER0_COMPA_vect ) {
+  // Increment our timer every time this ISR runs.
   timer0_millis++;
 }
 
 void init_ADC()
 {
+  // Below is modified from this extremely helpful page on ADC:
+  // https://www.marcelpost.com/wiki/index.php/ATtiny85_ADC
+
   /* this function initialises the ADC 
 
         ADC Prescaler Notes:
@@ -63,6 +71,9 @@ void init_ADC()
   DIDR0 |= (1 << ADC2D); //disable di on ADC pin
 }
 
+// Rather than import the whole arduino framework for one timer, we'll just setup a simple millis function.
+// Since we're using the internal oscillator, we need to account for the clock error.
+// This is done by adjusting the OCR0A value.
 void init_millis() {
   TCCR0A = (1<<WGM01); // CTC mode
   TCCR0B |= (1<<CS01); //clock/8 scaler
@@ -72,11 +83,15 @@ void init_millis() {
   TIMSK |= (1 << OCIE0A);
 }
 
+// Setup an interrupt for the crank signal
 void init_crank_interrupt() {
   PCMSK |= (1 << PCINT1); //PB6
   GIMSK |= (1<<PCIE); //enable pin change interrupts
 }
 
+// This is the millis function. Rather than read timer0_millis directly, we'll disable interrupts
+// while we read it. This is because timer0_millis is volatile and we don't want to read it
+// while it's being updated.
 unsigned long millis(void) {
   uint32_t millis_return;
   cli();
@@ -86,8 +101,9 @@ unsigned long millis(void) {
 }
 
 int main() {
-  DDRB |= (1<<DDB4) | (1<<PB0); //PB4 and PB0 as output
+  DDRB |= (1<<DDB4) | (1<<PB0); // Set PB4 and PB0 as outputs
   
+  // Disable interrupts while we initialize everything.
   cli();
   init_ADC();
   init_crank_interrupt();
@@ -99,34 +115,41 @@ int main() {
   uint8_t count = 0;
   uint16_t sum = 0;
 
-  while(count<10) {
+  // Take 10 ADC readings and sum them.
+  while( count < 10 ) {
     ADCSRA |= (1 << ADSC);         // start ADC measurement
-    while (ADCSRA & (1 << ADSC) ); // wait till conversion complete 
+    while ( ADCSRA & (1 << ADSC) ); // wait till conversion complete 
     sum += ADCH;
     count++;
     _delay_ms(1);
   }
-  uint8_t adc_val = sum/10;
-  uint32_t glow_time = glow_data[adc_val] * 1000;
+
+  // Average the 10 readings and use that to index into the glow_data array.
+  uint8_t adc_val = sum / 10;
+  uint32_t glow_time = glow_data[adc_val] * 1000; // Convert to ms
   uint32_t last_ms = 0;
 
-  if (glow_time > 0 ) {
+  if ( glow_time > 0 ) {
+    // Turn on the glow plug output for the calculated time.
     PORTB |= (1<<PB4);
-    while(millis() < glow_time) {
+    while( millis() < glow_time ) {
 
-      if (crank) {
+      // If the crank signal is received, turn off the glow plug and exit.
+      if ( crank ) {
         break;
       }
-      //heartbeat
+      // Heartbeat LED on/off every 1s.
+      // Handy for checking timer accuracy.
       if (millis() - last_ms > 500) {
         PORTB ^= _BV(PB0);
         last_ms = millis();
       }
     }
   }
-  //power down mode until power is cycled again
-  PORTB = 0; //turn off outputs
-  MCUCR |= (1<<SM1);
+
+  // Idle until power is cycled again
+  PORTB = 0; // Set all outputs low.
+  MCUCR |= (1<<SM1); // Sleep forever
 }
 
 
